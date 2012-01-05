b9234243b9ecc355737bcbf9cbca57959f1119df
Sylvain Alborini <sylvain.alborini@gmail.com>
2012-01-05 21:44:05 +0100

    reverse progress bar

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from kivy.core.audio import SoundLoader
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.animation import Animation
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty,NumericProperty
 from kivy.uix.progressbar import ProgressBar
 from random import choice,shuffle
 from glob import glob
@@ -25,6 +25,7 @@ class MemoryButton(Button):
     sound = ObjectProperty(None)
     background_normal = ObjectProperty(None)
     background = ObjectProperty(None)
+    
  
     def on_filenameSound(self, instance, value):
         # the first time that the filename is set, we are loading the sample
@@ -43,12 +44,7 @@ class MemoryButton(Button):
         cls.playsound = instance.state == 'normal'
 
     def on_press(self):
-        if self.parent.state=='OK' and not self.done:
-            # stop the sound if it's currently playing
-            #if self.sound.status != 'stop':
-            #    self.sound.stop()
-            #self.sound.play()
-            
+        if self.parent.state=='OK' and not self.done: 
             if self.parent.first is None:
                 self.parent.first = self
                 self.background_down,self.background_normal = self.background_normal,self.background_down
@@ -57,6 +53,7 @@ class MemoryButton(Button):
                     self.parent.first==None
                 elif self.parent.first.filenameIcon == self.filenameIcon:
                     print "youhou!!"
+                    self.parent.left+=1
                     if self.playsound:
                         if self.sound.status != 'stop':
                             self.sound.stop()
@@ -72,10 +69,12 @@ class MemoryButton(Button):
                      self.parent.first =None
 
 class MemoryLayout(GridLayout):
+    left = NumericProperty(0)
     def __init__(self, **kwargs):
         super(MemoryLayout, self).__init__(**kwargs)
         self.state = ""
         self.first=None
+        self.level=kwargs["level"]
 
     def hideButtons(self):
         for i in self.children:
@@ -87,13 +86,17 @@ class MyPb(ProgressBar):
     def __init__(self, **kwargs):
         super(ProgressBar, self).__init__(**kwargs)
         self.ml = kwargs['ml']
-        
+
     def updatePB(self,dt):
-        self.value+=10
+        self.value += 10*len(self.ml.children)/2
         if self.value==self.max:
             Clock.unschedule(self.updatePB)
             self.ml.hideButtons()
     
+    def foundAnItem(self,instance,value):
+        print self.max,self.value
+        self.value -= 100*self.ml.level
+
 def loadData():
     sounds={}
     icons=[]
@@ -112,14 +115,14 @@ class MyAnimalsApp(App):
     def build(self):
         sounds,icons=loadData()
         show=10
-        g = MemoryLayout(cols=len(icons)/2, size_hint=(1,.9))
+        g = MemoryLayout(cols=len(icons)/2, level=show,size_hint=(1,.9))
         config = BoxLayout(orientation='horizontal', size_hint=(1,.1))
         sound = ToggleButton(text='Sound On', size_hint=(0.1,1))
         sound.bind(on_press=MemoryButton.toggleSound)
-        pb = MyPb(max=100*show, size_hint=(0.9,1),ml=g)
+        pb = MyPb(max=len(icons)*100*show, size_hint=(0.9,1),ml=g)
         config.add_widget(pb)
         config.add_widget(sound)
-        
+        g.bind(left=pb.foundAnItem)
 
         root = BoxLayout(orientation='vertical')
         root.add_widget(g)
