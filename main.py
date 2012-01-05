d2e1e19d1218cf67136fcbba5df57796c4af5431
niavlys <sylvain.alborini@gmail.com>
2012-01-05 16:42:15 +0100

    TODO: scoring, game reload, etc

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 
 import kivy
 kivy.require('1.0.8')
-
-
-from random import random,choice,randint,shuffle
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.core.audio import SoundLoader
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.animation import Animation
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.progressbar import ProgressBar
+from random import choice,shuffle
 from glob import glob
 from os.path import dirname, join, basename
 
-def flatten(l):
-    return [item for sublist in l for item in sublist]
-
 class MemoryButton(Button):
     done=False
+    playsound=True
     filenameSound = StringProperty(None)
     filenameIcon = StringProperty(None)
     sound = ObjectProperty(None)
@@ -34,12 +33,15 @@ class MemoryButton(Button):
     
     def on_filenameIcon(self, instance, value):
         # the first time that the filename is set, we are loading the sample
-        #if self.icon is None:
-        #    self.icon = Image(source=value)
         if self.background_normal is None:
             self.background_normal=value
             self.background = value
-            
+   
+    @classmethod
+    def toggleSound(cls,instance):
+        instance.text = ["Sound On" if instance.state == 'normal' else "Sound Off"][0]
+        cls.playsound = instance.state == 'normal'
+
     def on_press(self):
         if self.parent.state=='OK' and not self.done:
             # stop the sound if it's currently playing
@@ -52,10 +54,13 @@ class MemoryButton(Button):
                 self.background_down,self.background_normal = self.background_normal,self.background_down
             else:
                 if self is self.parent.first:
-                    #self.background_down,self.background_normal = self.background_normal,self.background_down
                     self.parent.first==None
                 elif self.parent.first.filenameIcon == self.filenameIcon:
                     print "youhou!!"
+                    if self.playsound:
+                        if self.sound.status != 'stop':
+                            self.sound.stop()
+                        self.sound.play()
                     self.background_down,self.background_normal = self.background,self.background
                     self.parent.first.background_down,self.parent.first.background_normal = self.parent.first.background,self.parent.first.background
                     self.done=True
@@ -72,75 +77,67 @@ class MemoryLayout(GridLayout):
         self.state = ""
         self.first=None
 
-    def my_callback(self,dt):
-        #everything loaded
-        Clock.schedule_once(self.hideButtons, 2)
-
-    def hideButtons(self,dt):
+    def hideButtons(self):
         for i in self.children:
             i.background_down,i.background_normal = i.background_normal,i.background_down
         self.state="OK"
 
 
+class MyPb(ProgressBar):
+    def __init__(self, **kwargs):
+        super(ProgressBar, self).__init__(**kwargs)
+        self.ml = kwargs['ml']
+        
+    def updatePB(self,dt):
+        self.value+=10
+        if self.value==self.max:
+            Clock.unschedule(self.updatePB)
+            self.ml.hideButtons()
     
 def loadData():
     sounds={}
-    icons=[]#{}
+    icons=[]
     for s in glob(join(dirname(__file__),"sounds", '*.wav')):
         name=basename(s[:-4]).split("_")[0]
         if sounds.has_key(name):
             sounds[name].append(s)
         else:
             sounds[name]=[s]
-
     for i in glob(join(dirname(__file__),"icons", '*.gif')):
         icons.append(i)
-        #name=basename(i[:-4]).split("_")[0]
-        #if icons.has_key(name):
-        #    icons[name].append(i)
-        #else:
-        #    icons[name]=[i]
-    #print sounds
-    #print icons
     return sounds,icons
 
 class MyAnimalsApp(App):
 
     def build(self):
         sounds,icons=loadData()
+        show=10
+        g = MemoryLayout(cols=len(icons)/2, size_hint=(1,.9))
+        config = BoxLayout(orientation='horizontal', size_hint=(1,.1))
+        sound = ToggleButton(text='Sound On', size_hint=(0.1,1))
+        sound.bind(on_press=MemoryButton.toggleSound)
+        pb = MyPb(max=100*show, size_hint=(0.9,1),ml=g)
+        config.add_widget(pb)
+        config.add_widget(sound)
         
-        g = MemoryLayout(cols=6,)
-        """print root.size
-        for i in sounds:
-            for j in sounds[i]:
-                anIcon = choice(icons[i])
-                btn = AudioButton(
-                    text="",#i,
-                    filenameSound=j,
-                    filenameIcon=anIcon,
-                    size_hint=(None, None), halign='center',
-                    pos_hint={'x':random()*0.8,'y':random()*0.8},
-                    size=(128,128), text_size=(118, None)
-                    )
-                root.add_widget(btn)
-                """
+
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(g)
+        root.add_widget(config)
         icons=icons+icons
         shuffle(icons)
         for i in icons:
             aSound = choice(sounds[i.split("_")[0].split('/')[1]])
-            
             btn = MemoryButton(
-                text="",#i,
+                text="",
                 filenameIcon=i,
                 filenameSound=aSound,
-                #size=(128,128), text_size=(118, None)
-                )
-            
+                )  
             g.add_widget(btn)
 
-        Clock.schedule_once(g.my_callback, 0)
-        return g
-        #return root
+        Clock.schedule_interval(pb.updatePB,0.1)
+        #return g
+        return root
 
 if __name__ in ('__main__', '__android__'):
     MyAnimalsApp().run()
